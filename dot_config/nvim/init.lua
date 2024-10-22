54f609ff867b25a96a534e4e4bc30f40f0fcb8bd
Robert Chandler <robert@akchandler.com>
2024-10-22 12:00:16 -0700

    nvim: fix signature help

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -617,6 +617,7 @@ local on_attach = function(client, bufnr)
   -- See `:help K` for why this keymap
   nmap("K", vim.lsp.buf.hover, "Hover Documentation")
   nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+  vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, { buffer = bufnr, desc = "LSP: Signature Documentation" })
 
   -- Lesser used LSP functionality
   nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
