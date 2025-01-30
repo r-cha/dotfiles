750c3b79a9fa201548344572db1524a8bb1e123e
Robert Chandler <robert@akchandler.com>
2025-01-30 11:31:14 -0700

    nvim: remove some ensured lsps

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -558,17 +558,14 @@ vim.defer_fn(function()
   require("nvim-treesitter.configs").setup({
     -- Add languages to be installed here that you want installed for treesitter
     ensure_installed = {
-      "go",
       "lua",
       "python",
-      "rust",
       "tsx",
       "javascript",
       "typescript",
       "vimdoc",
       "vim",
       "bash",
-      "ocaml",
     },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
